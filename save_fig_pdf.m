e94c4ad15ab885b8bf59b0a3edc9307cdfa471ce
Daniel <danielcox16@hotmail.com>
2017-07-13 15:45:01 +0200

    Add comments save_fig_pdf

--- save_fig_pdf.m
+++ save_fig_pdf.m
@@ -1,26 +1,25 @@
 function save_fig_pdf(filename,h)
-% Save current figure to PDF
-% and crop edges.
+% Save figure to PDF and crop edges.
 %
 % Usages:
 %   save_fig_pdf
 %   save_fig_pdf(filename)
-%   save_fig_pdf(filename,fig)
+%   save_fig_pdf(filename,h)
 %
-% fig can be either the figure number or handler
+% h can be either the figure number or handler
 %
 % Default filename:  'figure'
 % Default figure:    current figure
 
-if nargin < 1
+if nargin < 1                   % Default filename
     filename = 'figure';        % Just call it 'figure'
 end
 
-if nargin < 2
+if nargin < 2                   % Default figure
     h = gcf;                    % Use current figure
 end
 
-set(h,'Units','Inches');
+set(h,'Units','Inches');        % Set figure units to inches
 pos = get(h,'Position');        % Get figure positions in inches
 set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
 print(h,filename,'-dpdf','-r0') % Write to PDF file
