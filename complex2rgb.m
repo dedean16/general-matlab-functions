b0bc379303b0037d7b5b2b70279afed9aa5741ab
Daniel <danielcox16@hotmail.com>
2019-01-18 14:29:10 +0100

    Add vbright to documentation complex2rgb

--- complex2rgb.m
+++ complex2rgb.m
@@ -3,28 +3,30 @@ function c = complex2rgb(z, varargin)
     % The output is intended for plotting with e.g. imagesc().
     %
     % Output:
-    % c:      N*M*3 array, representing the RGB color channels, where N*M
-    %         is the size of z.
+    % c:       N*M*3 array, representing the RGB color channels, where N*M
+    %          is the size of z.
     %
     % Input:
-    % z:      Complex 1D or 2D array
+    % z:       Complex 1D or 2D array
     %
     % Further options can be passed by either a struct or Parameter Value
     % pairs. In the following description, |z| denotes the absolute value
     % of the input argument z.
     %
     % Options:
-    % vscale: Scalar > 0 or 'auto'. Default 'auto'. Controls how the value
-    %         scales with |z|. Pass 'auto' to use the maximum of |z|.
-    % vgamma: Scalar > 0. Default 1. Gamma correction for value.
-    % vstep:  Scalar between (or equal to) 0 and 1. Default 0.
-    %         If vstep == 0, the value clamps at 1. If vstep > 0, the mod
-    %         of the value is taken, such that triangular steps from
-    %         (1-vstep) to 1 are created. This is useful for showing
-    %         amplitude contourlines along with phase.
-    % sscale: Scalar >= 0 or 'auto'. Default 0. Controls falloff of
-    %         saturation as a function of |z|. 1/sscale equals the Half
-    %         Width Half Maximum of the falloff curve.
+    % vscale:  Scalar > 0 or 'auto'. Default 'auto'. Controls how the value
+    %          scales with |z|. Pass 'auto' to use the maximum of |z|.
+    % vgamma:  Scalar > 0. Default 1. Gamma correction for value.
+    % vbright: Scalar. Default 0. Brightness correction for value computa-
+    %          tion. Final value will be multiplied by factor of 1+vbright.
+    % vstep:   Scalar between (or equal to) 0 and 1. Default 0.
+    %          If vstep == 0, the value clamps at 1. If vstep > 0, the mod
+    %          of the value is taken, such that triangular steps from
+    %          (1-vstep) to 1 are created. This is useful for showing
+    %          amplitude contourlines along with phase.
+    % sscale:  Scalar >= 0 or 'auto'. Default 0. Controls falloff of
+    %          saturation as a function of |z|. 1/sscale equals the Half
+    %          Width Half Maximum of the falloff curve.
     %
     % Example usage:
     % x = -2:0.1:2; y = x';
